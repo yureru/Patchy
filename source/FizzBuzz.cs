8cf29583b8de7f98773df486444ea12d43e7c931
roli <roli@cock.li>
2016-08-10 01:56:00 -0500

    Adding some memes.
    FizzBuzz and a Recursive FizzBuzz.

---
+++ source/FizzBuzz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+/// <summary>
+/// My first attempt ever to make a fizzbuzz.
+/// </summary>
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 100; ++i)
+        {
+            bool flag = false;
+            if (i % 3 == 0)
+            {
+                Console.Write("Fizz");
+                flag = true;
+            }
+            if (i % 5 == 0)
+            {
+                Console.Write("Buzz");
+                flag = true;
+            }
+            if (!flag)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ source/RecursiveFizzBuzz.cs
@@ -0,0 +1,41 @@
+﻿using System;
+/// <summary>
+/// Approach to write a recursive FizzBuzz. It's tail recursive, therefore it's TCO, but
+/// that depends on the CLR and C# Compiler.
+/// </summary>
+class Program
+{
+    static void Main()
+    {
+        string str = "";
+        Console.Write(FizzBuzz(1, str));
+    }
+
+    static string FizzBuzz(int num, string str)
+    {
+        bool flag = false;
+
+        if (num % 3 == 0)
+        {
+            str += "Fizz";
+            flag = true;
+        }
+        if (num % 5 == 0)
+        {
+            str += "Buzz";
+            flag = true;
+        }
+        if (!flag)
+        {
+            str += num.ToString();
+        }
+        str += "\n";
+
+        if (num < 100)
+        {
+            return FizzBuzz(num + 1, str);
+        }
+
+        return str;
+    }
+}
